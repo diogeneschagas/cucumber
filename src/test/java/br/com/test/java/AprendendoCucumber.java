7c23d4c0ad60af50e708cd3d38a13f1bb032c4f7
diogeneschagas <diogeneschagas20@gmail.com>
2020-12-08 23:15:49 -0300

    ajustes no primeiro teste

--- src/test/java/br/com/test/java/AprendendoCucumber.java
+++ src/test/java/br/com/test/java/AprendendoCucumber.java
@@ -6,18 +6,30 @@ import io.cucumber.java.en.When;
 
 public class AprendendoCucumber {
 
+    public void executarPassos(){
+        int quantidade = 3;
+        int [] quantPassos = new int[quantidade];
+        for (int i = 0; i < quantidade; i++){
+            System.out.println("Passo" + (i+1));
+        }
+
+    }
+
     @Given("que criei o arquivo corretamente")
     public void que_criei_o_arquivo_corretamente() {
-        System.out.println("Passo1");
+        executarPassos();
+//        System.out.println("Passo1");
     }
 
     @When("executar")
     public void executar() {
-        System.out.println("Passo2");
+        executarPassos();
+//        System.out.println("Passo2");
     }
 
     @Then("a especificacao deve finalizar com sucesso")
     public void a_especificacao_deve_finalizar_com_sucesso() {
-        System.out.println("Passo3");
+        executarPassos();
+//        System.out.println("Passo3");
     }
 }
