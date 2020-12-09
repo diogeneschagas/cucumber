2168d7e2c69dbe2c0050ac77ed87b90e4464a5a0
diogeneschagas <diogeneschagas20@gmail.com>
2020-12-08 23:27:14 -0300

    remocao de espacos

--- src/test/java/br/com/test/java/RunnerTest.java
+++ src/test/java/br/com/test/java/RunnerTest.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = "pretty",
-        features = {"src/test/java/resources"})
+        features = {"src/test/java/resources"}
+        )
 public class RunnerTest {
 
 }
