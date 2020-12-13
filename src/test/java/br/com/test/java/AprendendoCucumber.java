9b71cd39960913f256964274e34b43f3e7524879
diogeneschagas <diogeneschagas20@gmail.com>
2020-12-12 21:26:22 -0300

    Expressões regulares

--- src/test/java/br/com/test/java/AprendendoCucumber.java
+++ src/test/java/br/com/test/java/AprendendoCucumber.java
@@ -3,12 +3,16 @@ package br.com.test.java;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class AprendendoCucumber {
 
-    public void executarPassos(int numPasso){
-            System.out.println("Passo " + numPasso);
-        }
+    /**
+     * Cenário 1: Aprendendo a escrever cenários
+     **/
+    public void executarPassos(int numPasso) {
+        System.out.println("Passo " + numPasso);
+    }
 
     @Given("que criei o arquivo corretamente")
     public void queCrieiOArquivoCorretamente() {
@@ -19,12 +23,35 @@ public class AprendendoCucumber {
     public void executar() {
         executarPassos(2);
     }
+
     @Then("a especificacao deve finalizar com sucesso")
     public void aEspecificacaoDeveFinalizarComSucesso() {
         executarPassos(3);
     }
+
     @Then("passo enxerido")
     public void passoEnxerido() {
         executarPassos(4);
+    }
+
+    /**
+     * Cenário 2: Implementando um contador
+     **/
+
+    private int contador = 0;
+
+    @Given("que o valor do contador eh {int}")
+    public void queOValorDoContadorEh(int arg1) {
+        contador = arg1;
+    }
+
+    @When("eu incrementar em {int}")
+    public void euIncrementarEm(int arg1) {
+        contador += arg1;
+    }
+
+    @Then("o valor do contador sera {int}")
+    public void oValorDoContadorSera(int arg1) {
+        Assert.assertEquals(arg1,contador);
     }
 }
--- src/test/java/resources/aprendendo_cucumber.feature
+++ src/test/java/resources/aprendendo_cucumber.feature
@@ -8,3 +8,8 @@ Scenario: Deve executar a especificacao
   When executar
   Then a especificacao deve finalizar com sucesso
   Then passo enxerido
+
+Scenario: Deve incrementar contador
+  Given que o valor do contador eh 15
+  When eu incrementar em 3
+  Then o valor do contador sera 18
